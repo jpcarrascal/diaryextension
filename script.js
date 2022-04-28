3328c06e716b5fbfacc745293ff178db6d53ae8b
JP Carrascal <juanpcarrascal@gmail.com>
2022-04-28 21:59:29 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,48 +1,45 @@
-//if( key.indexOf("domestika.org") > -1 )
-console.log("I'm 1her")
-var minTimeToPrompt = 20000; // 10 seconds
 var diaryURL = "http://www.jpcarrascal.com";
 var browser = window.browser || window.chrome;
+//var matchURL = browser.runtime.getManifest().content_scripts[0].matches;
+//let domain = (new URL(matchURL)).host.replace("www.","");
+var minTimeToPrompt = 20000; // 20 seconds
 //chrome.storage.sync.clear();
 window.onload = function() {
     var now = new Date().getTime();
     var url = location.protocol + '//' + location.host + location.pathname; // Omit query from URL
-    if( url.indexOf("domestika.org") > -1 ) {
-        console.log("I'm there")
-        browser.storage.sync.get(null, function(result){
-            var visited = result[url] || null;
-            console.log("Last visit:" + (visited?visited.last:"never") );
-            if(visited == null || (now - visited.last > minTimeToPrompt) ) {
-                diaryPrompt(url, now);
-                var obj = {};
-                obj[url] = {last: now};
-                browser.storage.sync.set(obj, function() {
-                    console.log("Updating last visited time: " + now);
-                });
-            } else {
-                console.log("Too little time has passed...")
-            }
-        });
 
-/*
-        switch(window.location.href) {
-            case "https://www.domestika.org/":
-                if(Math.random() > 0.5)
-                    console.log("Pagina principal");
-                break;
-            case "https://www.domestika.org/es/courses":
-                console.log("Pagina de cursos");
-                break;
-            default:
-                break;
-        }
-*/
-    } 
+    browser.storage.sync.get(["username"], function(result){
+        if(!result.username) {
+            let username = prompt("This is the first time you use the Diary. Please enter your username.");
+            browser.storage.sync.set({username: username}, function() {
+
+            });
+        } else {
+            browser.storage.sync.get(null, function(result){
+                var visited = result[url] || null;
+                console.log("Last visit:" + (visited?visited.last:"never") );
+                if(visited == null || (now - visited.last > minTimeToPrompt) ) {
+                    diaryPrompt(url, now, result.username);
+                    var obj = {};
+                    obj[url] = {last: now};
+                    browser.storage.sync.set(obj, function() {
+                        console.log("Updating last visited time: " + now);
+                    });
+                } else {
+                    console.log("Too little time has passed...")
+                }
+            });
+        }    
+    });
+
+
 }
 
-function diaryPrompt(url, timestamp) {
+function diaryPrompt(url, timestamp, username) {
     //let sign = prompt("What's your sign?");
-    var destination = diaryURL + "?url=" + url + "&timestamp=" + timestamp;
+    var destination = diaryURL + "?url=" + url +
+                    "&timestamp=" + timestamp +
+                    "&username=" + username;
     if ( window.confirm("Do you want to complete a diary entry now?") )
     {
         window.open(destination, "_blank");
